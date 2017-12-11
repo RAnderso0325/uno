04aaed50f4d2362cc270a4f39d455f67817f3d28
RAnderso0325 <reillyjanderson@gmail.com>
2017-12-11 15:19:05 -0800

    sub html and created card deck

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//create player hand
+
+//shuffle deck, ie randomly add seven cards from the deck to player 1 hand
+//display cards for player 1
+
+//add a card to the discard pile
+
+//game play:
+
+//
