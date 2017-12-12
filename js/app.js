35df9aa7bc56ebed6108e5a8694bdba04c5a6ce2
RAnderso0325 <reillyjanderson@gmail.com>
2017-12-12 14:25:46 -0800

    player hands do not originally display

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var turn = 0; //current index
 var shuffledDeck;
 var reverse = false;
 var discardPile = [];
+var currentPlayer = playerArr[0];
+// var playerInPlay;
 
 //set game board
 function setGameBoard(){
@@ -27,14 +29,17 @@ function shuffleDeck(){
 function dealCards(){
 	console.log("player hand dealt");
     for(var i=0; i<playerArr.length; i++){
-    	var playerInPlay = document.createElement('h2');
-    	playerInPlay.textContent = "Player "+(i+1);
-    	document.getElementById('game-board').appendChild(playerInPlay);
+    	var playerInPlay = document.createElement('div');
+    	playerInPlay.setAttribute('id', 'player'+i);
+    	var playerTitle = document.createElement('h2');
+    	playerTitle.textContent = "Player "+(i+1);
+    	playerInPlay.appendChild(playerTitle);
     	playerArr[i].playerHand = shuffledDeck.splice(0,7);
     	var hand = document.createElement('div');
     	hand.setAttribute('class', 'hand');
-    	hand.setAttribute('id', 'player'+i);
-    	document.getElementById('game-board').appendChild(hand);
+    	// hand.setAttribute('id', 'player'+i);
+    	playerInPlay.style.display = "none";
+    	document.getElementById('game-board').appendChild(playerInPlay).appendChild(hand);
     	// playerHand();
     }
     discardPile = shuffledDeck.splice(0,1);
@@ -54,6 +59,7 @@ function addPlayerHand(){
 			card.classList.add(playerHand[j].value);
 			card.textContent=playerHand[j].value;
 			card.style.backgroundColor = playerHand[j].color;
+			card.addEventListener('click', playCard);
 			document.getElementById('player'+i).appendChild(card);
 		}
 	}
@@ -64,6 +70,7 @@ function createDiscardPile(){
 	var discardPileTitle = document.createElement('h2');
 	discardPileTitle.textContent = "Discard Pile";
 	document.getElementById('game-board').appendChild(discardPileTitle);
+
 	var discardCard = document.createElement('div');
 	discardCard.setAttribute('class', 'card');
 	discardCard.classList.add(discardPile[0].color);
@@ -76,14 +83,18 @@ function createDiscardPile(){
 function playerTurnIs(){
 	if(reverse === false){
 		turn++;
-		if(turn>playerArr.length){
+		if(turn>=playerArr.length){
 			turn=0;
 		}
+		currentPlayer = playerArr[turn];
+		document.getElementById('player'+turn).style.display = "inline-block";
 	}else{
 		turn--;
-		if(turn<0){
+		if(turn<=0){
 			turn=playerArr.length-1;
 		}
+		currentPlayer = playerArr[turn];
+		
 	}
 	console.log("current turn is",playerArr[turn]);
 }
@@ -91,23 +102,38 @@ function playerTurnIs(){
 //card must match either color or value to be played
 function playCard(){
 	console.log("card is played");
-	//move card to discard pile
+	//check if card value or color equals that of the one in discardPile[0]
+
+	//if it is not equal then prompt the user to play a card or draw a card
+
+	//if it is equal...
+
+	//push card into discard array
+
+	//pop card from playerHand array
+
+	//make the style display none again
+
+	playerTurnIs();
 }
 
 //draw logic
 //include draw card button
-//prompt user to draw a card if they try to play a card that doesn't match the value or color requirements
 function drawCard(){
 	console.log("player would like to draw a card");
 	//add card to player hand
 }
 
 function reset(){
-	console.log("play a new game");
+	console.log("still need to add clear current board functionality");
+	//clear current game board
+	//clear player hand in player arrays
+	//ask how many players from user input
+	setGameBoard();
 }
 
 document.addEventListener("DOMContentLoaded", function(){
 	document.getElementById('new-game').addEventListener('click', reset);
 	document.getElementById('draw-card').addEventListener('click', drawCard);
 	setGameBoard();
-})
+});
--- js/player.js
+++ js/player.js
@@ -11,3 +11,11 @@ var playerArr = [
 	playerName: "c",
 	playerHand: [],
 }];
+
+//modal with number of players
+//radio buttons with 2 3 4
+//when it is clicked, 
+//pops up with player 1, player 2 etc etc
+//once all the names are in
+//populate playerArr.push({playerName: "entry1", playerHand:[]})
+//radio group . click depending on which is clicked, show that many boxes below
